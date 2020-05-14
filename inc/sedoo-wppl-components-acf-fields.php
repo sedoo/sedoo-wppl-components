2577cf7ea7f5dccc362dab6c28d29bc826d3437b
Pierre Vert <pierr65@gmail.com>
2020-05-14 15:36:18 +0200

    limit to one component per block, add support large width

--- inc/sedoo-wppl-components-acf-fields.php
+++ inc/sedoo-wppl-components-acf-fields.php
@@ -110,7 +110,7 @@ acf_add_local_field_group(array(
                 ),
                 'elements' => '',
                 'min' => '',
-                'max' => '',
+                'max' => '1',
                 'return_format' => 'object',
             ),
         ),
