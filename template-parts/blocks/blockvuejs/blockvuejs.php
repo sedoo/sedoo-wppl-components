b9ee99aebffebc95553c22db4c2399834345d73f
ngruwe <nicolas.gruwe@obs-mip.fr>
2021-03-19 14:03:05 +0100

    fix the lang used in block render

--- template-parts/blocks/blockvuejs/blockvuejs.php
+++ template-parts/blocks/blockvuejs/blockvuejs.php
@@ -21,21 +21,23 @@ $IdElementVueJS = $blockvuejs[0];
 ?>
 <section class="sedoo-component <?php echo $className;?>">
 <?php
-echo get_field('contenu_du_block', $IdElementVueJS);
+
+$nom_campagne = get_field('nom_de_la_campagne', 'option');
+$campaign_replaced = str_replace('$$CAMPAIGNNAME$$', $nom_campagne, get_sub_field('contenu_du_block', $IdElementVueJS));
+            
+$current_lang = substr( get_bloginfo ( 'language' ), 0, 2 );
+if ( function_exists('pll_the_languages') ) {
+    $current_lang = pll_current_language();
+}
+
+
+echo str_replace('$$LANGUAGE$$', $current_lang, $campaign_replaced);
+
 // load script after content block
 if( have_rows('elements_inclus', $IdElementVueJS) ):
     while( have_rows('elements_inclus', $IdElementVueJS) ): the_row(); 
         if(get_field('nom_de_la_campagne', 'option')) {
-            $nom_campagne = get_field('nom_de_la_campagne', 'option');
-            $campaign_replaced = str_replace('$$CAMPAIGNNAME$$', $nom_campagne, get_sub_field('script'));
-            
-            $current_lang = substr( get_bloginfo ( 'language' ), 0, 2 );
-            if ( function_exists('pll_the_languages') ) {
-                $current_lang = pll_current_language();
-            }
-
-
-            echo str_replace('$$LANGUAGE$$', $current_lang, $campaign_replaced);
+            echo str_replace('$$CAMPAIGNNAME$$', $nom_campagne, get_sub_field('script'));
         } else {
             echo the_sub_field('script');
         }
